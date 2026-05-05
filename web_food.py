85915918b1b5a594627c83517ea5c4cf59e2b0db
thaisaraujo0819-sudo <thaisaraujo.0819@gmail.com>
2026-05-05 17:14:34 -0300

    Update web_food.py

--- web_food.py
+++ web_food.py
@@ -9,7 +9,10 @@ def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
-
+    
+@app.route("/")
+def home():
+    return "API online "
 
 
 app = Flask(__name__)
