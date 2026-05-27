16552d7a1a2f72a305818b355394b7f211242515
thaisaraujo0819-sudo <thaisaraujo.0819@gmail.com>
2026-05-27 11:16:27 -0300

    Update web_food.py

--- web_food.py
+++ web_food.py
@@ -34,7 +34,17 @@ def index():
 def nutriplanner():
     return  render_template("nutriplanner.html")
     
+# segurança
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
+# email
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_USERNAME")
 
 mail = Mail(app)
 
